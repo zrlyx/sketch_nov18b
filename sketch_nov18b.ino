05e5c4b269c9a70c7ca6604f1149c700b7957b34
zrlyx <20141105042hyb@admindeiMac-007.local>
2015-11-18 21:05:04 +0800

    sgd
    huvtytftf7tuc7f7ctu

---
+++ sketch_nov18b.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  delay(2500);
+  digitalWrite(13,LOW);
+  delay(2500);
+  digitalWrite(13,HIGH);
+  delay(50);
+  digitalWrite(13,LOW);
+  delay(50);
+  
+}
